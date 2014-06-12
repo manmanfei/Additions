59e8da3ff15578a33ab8ed35b2d6636d989da936
hongfei <wohudie@gmail.com>
2014-06-12 16:56:14 +0800

    将增加的文件加入AdditionsMacro

--- Additions/Additions/library/AdditionsMacro.h
+++ Additions/Additions/library/AdditionsMacro.h
@@ -34,6 +34,8 @@
 #import "UIView+Additions.h"
 #import "UIColor+Additions.h"
 
+#import "UIView+ViewLayout.h"
+
 // 颜色宏
 #define kGetColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1]
 
